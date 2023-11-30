151251dfd4138d5bdf5c14162cb7b5fb7c66b90b
cryptojang <robert.crypto.jang@gmail.com>
2023-11-30 09:44:21 +0900

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 const App = () => {
   return (
-    <div className="bg-red-100">
-      <div>Hello</div>
+    <div className="bg-green-950 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl text-white font-semibold animate__animated animate__delay-3s	mb-12">
+        라스블랑카스의 두목 춘짱
+      </h1>
+      <img
+        className="w-96 h-96 animate__animated animate__zoomIn animate__verySlow	"
+        src="./images/cat.jpg"
+        alt="cat"
+      />
     </div>
   );
 };
